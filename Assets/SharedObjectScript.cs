838872ae9c3d0f9625b8b4e83d698a641d82fe0f
Forrest Vanderborgh <forrestvanderborgh@gmail.com>
2018-03-11 20:08:04 -0700

    Create two example prefabs

---
+++ Assets/SharedObjectScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharedObjectScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
