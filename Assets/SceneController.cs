836f31ecd7136db3228deb59c8b58af5f203088c
Forrest Vanderborgh <forrestvanderborgh@gmail.com>
2018-03-11 19:36:41 -0700

    Fix misspelling in debug log

--- Assets/SceneController.cs
+++ Assets/SceneController.cs
@@ -6,7 +6,7 @@ public class SceneController : MonoBehaviour {
 
     // Use this for initialization
     void Start() {
-        Debug.Log("Henlo World!");
+        Debug.Log("Hello World!");
     }
 	
 	// Update is called once per frame
